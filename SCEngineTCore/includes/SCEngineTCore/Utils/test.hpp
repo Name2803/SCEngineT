609d22fbbcc93e19519e5677c3d383153379957f
Name <no.name.28.03.02021@gmail.com>
2021-07-14 23:29:12 +0500

    03 lesson adding glfw

---
+++ SCEngineTCore/includes/SCEngineTCore/Utils/test.hpp
@@ -0,0 +1,3 @@
+namespace SCEngineT {
+	int chekGLFW();
+}
---
+++ SCEngineTEditor/src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include <SCEngineTCore/Utils/test.hpp>
+
+int main()
+{
+	std::cout << "Hi!\n";
+	SCEngineT::chekGLFW();
+}
